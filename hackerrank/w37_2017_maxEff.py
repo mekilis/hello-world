f0a9d448429f8a12520eb398c7cbf06fabddcaf7
mekilis <st.nonso@gmail.com>
2018-04-21 23:09:05 +0100

    maxeff - passed testcases 0-3, tle for others

--- hackerrank/w37_2017_maxEff.py
+++ hackerrank/w37_2017_maxEff.py
@@ -3,27 +3,83 @@
 import os
 import sys
 import ast
+import itertools as it
 
 
 # Complete the maximumEfficiency function below.
 def maximumEfficiency(n, m):
     G = {}
-    Glist = []
+    Glist = () # should be a tuple
+    #fl = open('demo.txt', 'r')
+    #line = fl.readline() #discard
     for i in range(m):
         # O(n)
         kf = input().split()
         k, f = int(kf[0]), int(kf[1])
-        g = input().split()
-        G[repr(g)] = f
+        g = [int(j) for j in input().split()]
+        key = repr(g)
+        if key in G:
+            G[key] += f
+        else:
+            G[key] = f
+        #print(g)
+        #print(repr(g))
         #G[repr(g) + "*"] = k
-    print(G)
+    #print(G)
 
-    x = G.keys()
-    print("x", x)
-    GList = list(set().union(list(x)))
-    print(GList)
+    gkeys = G.keys()
+    gkeys = list(gkeys)
+    #print("gkeys", gkeys)
+    Keys = []
+    for i in range(len(gkeys)):
+        Keys.append(ast.literal_eval(gkeys[i]))
+    #print(Keys)
+    Glist = tuple(set().union(*Keys))
+    Gset = set(Glist)
+    #print(Glist)
 
-    return 0
+    # code begins
+    #Map = {} # prevent duplicates
+    maxi = 0
+    for i in range(len(Glist)):
+        gl = list(it.combinations(Glist, i+1))
+        #print(gl)
+        # O(n^2)
+        for j in range(len(gl)):
+            summ = 0
+            g_left = list(gl[j])
+            if len(g_left) == 0:
+                continue
+            g_left_set = set(g_left)
+            # remove common
+            g_right = list(set.union(g_left_set, Gset) - set.intersection(g_left_set, Gset))
+            if len(g_right) == 0:
+                continue
+            #print(g_left, g_right)
+            left, right = repr(g_left), repr(g_right)
+
+            # O(n^3) terrible
+            items = G.items()
+            for k, v in items:
+                kk = ast.literal_eval(k)
+                #print("...", k, kk, v)
+                #print(type(k), type(kk))
+                if set(kk).issubset(g_left):
+                    summ += v
+                if set(kk).issubset(g_right):
+                    summ += v
+            
+            '''if left in G:
+                summ += G[left]
+            if right in G:
+                summ += G[right]'''
+            if summ > maxi:
+                maxi = summ
+            #(summ, maxi)
+            
+    #fl.close()
+
+    return maxi
 
 if __name__ == '__main__':
 
@@ -35,7 +91,7 @@ if __name__ == '__main__':
 
     result = maximumEfficiency(n, m)
 
-    #print(str(result) + '\n')
+    print(str(result) + '\n')
 
 
 '''
